9c383d6621628438a92d95c62d36e3ca53caec1a
Thiago Beier <146458544+thiagogbeier@users.noreply.github.com>
2025-09-05 21:11:49 +0000

    Clean up debug console.log statements

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,5 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-console.log('Main.jsx is loading...')
-
 const root = createRoot(document.getElementById('root'))
-console.log('Root created, rendering App...')
 root.render(<App />)
